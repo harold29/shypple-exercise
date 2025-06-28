f4921ce00e49c0da7b2a5c7f9c3f091b95525dda
Harold Perez <haroldjose2903@gmail.com>
2025-06-27 22:39:05 -0300

    [002] Improvement in dijkstra algorithm, missing route reconstruction and improve cost calculation

--- map_calculator.rb
+++ map_calculator.rb
@@ -3,6 +3,57 @@
 require_relative 'graph_builder'
 require_relative 'file_data_loader'
 
+def dijkstra(graph, start, end_node, criteria)
+  distances = {}
+  previous = {}
+  visited = {}
+  nodes = graph.keys
+
+  nodes.each { |node| distances[node] = Float::INFINITY }
+  distances[start] = 0
+
+  until nodes.empty?
+    min_node = nodes.min_by { |node| visited[node] ? Float::INFINITY : distances[node] }
+
+    break if distances[min_node] == Float::INFINITY
+    break if min_node == end_node
+
+    visited[min_node] = true
+    nodes.delete(min_node)
+
+    graph[min_node].connection_hash.each do |neighbor, connections|
+      next if visited[neighbor]
+
+      best_connection = nil
+      best_cost = Float::INFINITY
+
+      connections.each do |connection|
+        case criteria
+        when 'cheapest'
+          cost = connection.rate
+        when 'fastest'
+          cost = (connection.arriving_date - connection.departing_date).to_i
+        else
+          next
+        end
+
+        if cost < best_cost
+          best_cost = cost
+          best_connection = connection
+        end
+      end
+
+      next unless best_connection
+
+      alt = distances[min_node] + best_cost
+      if alt < distances[neighbor]
+        distances[neighbor] = alt
+        previous[neighbor] = min_node
+      end
+    end
+  end
+end
+
 loaded_file = FileDataLoader.load_data('response.json')
 graph = GraphBuilder.build_graph(loaded_file.sailings, loaded_file.rates)
 exchange_rates = loaded_file.exchange_rates
@@ -24,37 +75,5 @@ while true
     puts 'fastest'
   else
     puts 'Invalid criteria'
-  end
-end
-
-def dijkstra(graph, start, end_node, criteria)
-  distances = {}
-  visited = {}
-  nodes = graph.keys
-
-  nodes.each { |node| distances[node] = Float::INFINITY }
-  distances[start] = 0
-
-  until nodes.empty?
-    min_node = nodes.min_by { |node| visited[node] ? Float::INFINITY : distances[node] }
-
-    break if distances[min_node] == Float::INFINITY
-    break if min_node == end_node
-
-    visited[min_node] = true
-    nodes.delete(min_node)
-
-    graph[min_node].connection_hash.each do |neighbor, connections|
-      connections.each do |connection|
-        case criteria
-        when 'cheapest'
-          cost = connection.rate
-        when 'fastest'
-          cost = (connection.arriving_date - connection.departing_date).to_i
-        else
-          next
-        end
-      end
-    end
   end
 end
